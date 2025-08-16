30ea1bcffc2c30249162a53986c29b9a80426426
HandsomeHarry <harryyu2002@gmail.com>
2025-08-16 23:56:28 +0800

    v0.1.0

--- test_fix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /mnt/e/cc/src-tauri
-echo "Building application..."
-cargo build 2>&1 | tail -20
-echo "Build complete. Running application..."
-timeout 30 cargo run 2>&1 | grep -A5 -B5 "DEBUG\|ERROR\|panic\|Generate"
